602f8b5d020f0c25d744902a9637da36e122dd8f
David Ovčačík <david.ovcacik@isobar.com>
2019-07-11 02:59:38 +0200

    Update docs, add t function

--- test/t.js
+++ test/t.js
@@ -1,10 +1,19 @@
 const {t} = require('../constructs');
 const assert = require('assert');
+const randomer = require('../util/randomer');
 
-/*describe('t - True', function() {
-	it('Should output true', function() {
+describe('t - Concat', function() {
+	it('Should push other arguments into first argument', function() {
+		const randomArray = randomer.array(randomer.anything);
+		const randomElement = randomer.anything();
 		const testCases = [
-			{output: () => t(), expected: true},
+			{output: () => t([]), expected: []},
+			{output: () => t([1]), expected: [1]},
+			{output: () => t([1], randomElement), expected: [1, randomElement]},
+			{output: () => t([2, 3], 4, 5, 6), expected: [2, 3, 4, 5, 6]},
+			{output: () => t([4, 5, 6, 7], 8, 9), expected: [4, 5, 6, 7, 8, 9]},
+			{output: () => t([0], [1, 2], [3, 4, 5]), expected: [0, [1, 2], [3, 4, 5]]},
+			{output: () => t(randomArray, randomElement), expected: [...randomArray, randomElement]},
 		];
 
 		for (let i = 0, length = testCases.length; i < length; i++) {
@@ -14,5 +23,5 @@ const assert = require('assert');
 
 			assert.deepStrictEqual(output, expected, `Test case #${i}: ${output} should equal ${expected}`);
 		}
-	})
-});*/
+	});
+});
