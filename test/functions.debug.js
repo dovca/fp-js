b2badb5ef80c563ff234b74ec06111dc97cb2947
David Ovčačík <david.ovcacik@isobar.com>
2019-07-15 00:20:43 +0200

    Correct implementation of function u

--- test/functions.debug.js
+++
@@ -1,4 +0,0 @@
-const fn = require('./sources/all')('functions.debug');
-for (const name in fn) {
-	fn[name]();
-}
--- test/sources/u.js
+++ test/sources/u.js
@@ -1,13 +1,64 @@
 module.exports = (functionsModuleName) => () => {
 	const {u} = require(`${__dirname}/../../lib/${functionsModuleName}`);
 	const assert = require('assert');
+	const {spy} = require('sinon');
 
 	describe(`${functionsModuleName}/u - Reduce`, function () {
 		const sum = (a, b) => a + b;
 		const or = (a, b) => a || b;
+		const not = (a) => !a;
+
+		it('Should work without arguments', function () {
+			const testCases = [
+				{output: () => u(), expected: undefined},
+			];
+
+			for (let i = 0, length = testCases.length; i < length; i++) {
+				const tc = testCases[i];
+				const output = tc.output();
+				const expected = tc.expected;
+
+				assert.deepStrictEqual(output, expected, `Test case #${i}: ${output} should equal ${expected}`);
+			}
+		});
+
+		it('Should work with function and no other arguments', function () {
+			const spiedOr = spy(or);
+			const spiedU = spy(u);
+
+			spiedU(spiedOr);
+			assert.deepStrictEqual(spiedOr.notCalled, true, `function passed to u should not have been called`);
+			assert.deepStrictEqual(spiedU.getCall(0).returned(undefined), true, `Expected u() to return undefined`);
+		});
+
+		it('Should work with function and one argument', function () {
+			const spiedOr = spy(or);
+			const spiedNot = spy(or);
+			const spiedSum = spy(or);
+
+			const testCases = [
+				{output: () => u(spiedOr, true), expected: true},
+				{output: () => u(spiedOr, false), expected: false},
+				{output: () => u(spiedNot, true), expected: true},
+				{output: () => u(spiedSum, 1), expected: 1},
+				{output: () => u(spiedSum, 50), expected: 50},
+				{output: () => u(spiedSum, 0), expected: 0},
+			];
+
+			for (let i = 0, length = testCases.length; i < length; i++) {
+				const tc = testCases[i];
+				const output = tc.output();
+				const expected = tc.expected;
+
+				assert.deepStrictEqual(output, expected, `Test case #${i}: ${output} should equal ${expected}`);
+			}
+
+			assert.deepStrictEqual(spiedOr.notCalled, true, `function 'or' passed to u should not have been called`);
+			assert.deepStrictEqual(spiedNot.notCalled, true, `function 'not' to u should not have been called`);
+			assert.deepStrictEqual(spiedSum.notCalled, true, `function 'sum to u should not have been called`);
+		});
 
 		it('Should work with function and two arguments', function () {
-
 			const testCases = [
 				{output: () => u(or, true, true), expected: true},
 				{output: () => u(or, false, true), expected: true},
